e4b9acef39af8b53e0315344447d47ba17f1e30f
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-07-18 15:13:25 -0300

    test: ensure LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,41 @@
+import { Collection } from 'mongodb';
+
+import { MongoHelper } from '../helpers/mongo-helper';
+import { LogMongoRepository } from './log';
+
+interface SutTypes {
+  sut: LogMongoRepository
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new LogMongoRepository();
+
+  return {
+    sut,
+  };
+};
+
+describe('Log Mongo Repository', () => {
+  let errorCollection: Collection;
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection('errors');
+    await errorCollection.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  it('should create an error log on success', async () => {
+    const { sut } = makeSut();
+    await sut.logError('any_error');
+
+    const count = await errorCollection.countDocuments();
+    expect(count).toBe(1);
+  });
+});
