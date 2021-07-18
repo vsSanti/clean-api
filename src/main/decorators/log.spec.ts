11ba16d940dd2de226e2a6cf52eb94e057bec56c
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-07-18 14:12:30 -0300

    test: ensure LogControllerDecorator returns the same result of the controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -50,4 +50,27 @@ describe('LogController Decorator', () => {
 
     expect(handleSpy).toHaveBeenCalledWith(httpRequest);
   });
+
+  it('should return the same result of the controller', async () => {
+    const { sut } = makeSut();
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Vinicius',
+      },
+    });
+  });
 });
