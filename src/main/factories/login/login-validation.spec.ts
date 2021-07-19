97618f05a842203a88a12acf3243f7af8a4435c8
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-07-18 21:15:46 -0300

    refactor: move Validation to protocols folder

--- src/main/factories/login/login-validation.spec.ts
+++ src/main/factories/login/login-validation.spec.ts
@@ -3,7 +3,7 @@ import {
   EmailValidation,
   RequiredFieldValidation,
 } from '../../../presentation/helpers/validators';
-import { Validation } from '../../../presentation/helpers/validators/validation';
+import { Validation } from '../../../presentation/protocols/validation';
 import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite';
 import { EmailValidator } from '../../../presentation/protocols/email-validator';
 import { makeLoginValidation } from './login-validation';
--- src/main/factories/signup/signup-validation.spec.ts
+++ src/main/factories/signup/signup-validation.spec.ts
@@ -4,7 +4,7 @@ import {
   EmailValidation,
   RequiredFieldValidation,
 } from '../../../presentation/helpers/validators';
-import { Validation } from '../../../presentation/helpers/validators/validation';
+import { Validation } from '../../../presentation/protocols/validation';
 import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite';
 import { EmailValidator } from '../../../presentation/protocols/email-validator';
 import { makeSignUpValidation } from './signup-validation';
