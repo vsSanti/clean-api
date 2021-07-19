b4704af7f63d88446e9702555b1b8282e875f581
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-07-19 20:35:28 -0300

    test: ensure AccountMongoRepository returns null if loadByEmail returns null

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -55,4 +55,11 @@ describe('Account Mongo Repository', () => {
     expect(account.email).toBe('any_email@email.com');
     expect(account.password).toBe('any_password');
   });
+
+  it('should return null if loadByEmail returns null', async () => {
+    const sut = makeSut();
+    const account = await sut.loadByEmail('any_email@email.com');
+
+    expect(account).toBeFalsy();
+  });
 });
