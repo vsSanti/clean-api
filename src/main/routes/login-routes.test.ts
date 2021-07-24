28f6d9f0467bae7bdee559df31faf6a8d604a81c
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-07-24 12:19:45 -0300

    refactor: rename signup-routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -3,7 +3,7 @@ import request from 'supertest';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 import app from '../config/app';
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
   });
@@ -17,15 +17,21 @@ describe('SignUp Routes', () => {
     await MongoHelper.disconnect();
   });
 
-  it('should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Vinicius',
-        email: 'viniciussdsilva@gmail.com',
-        password: '123',
-        passwordConfirmation: '123',
-      })
-      .expect(201);
+  describe('Login Routes', () => {
+
+  });
+
+  describe('POST /signup', () => {
+    it('should return 201 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Vinicius',
+          email: 'viniciussdsilva@gmail.com',
+          password: '123',
+          passwordConfirmation: '123',
+        })
+        .expect(201);
+    });
   });
 });
