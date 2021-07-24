0a2ff1ff5b72acef25c297d5c30e5aea9c0a3623
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-07-24 19:30:22 -0300

    refactor: split factories in folders

--- src/main/factories/controllers/login/login-validation-factory.spec.ts
+++ src/main/factories/controllers/login/login-validation-factory.spec.ts
@@ -1,15 +1,14 @@
 
 import {
-  CompareFieldsValidation,
   EmailValidation,
   RequiredFieldValidation,
   ValidationComposite,
-} from '../../../presentation/helpers/validators';
-import { Validation } from '../../../presentation/protocols/validation';
-import { EmailValidator } from '../../../presentation/protocols/email-validator';
-import { makeSignUpValidation } from './signup-validation-factory';
+} from '../../../../presentation/helpers/validators';
+import { Validation } from '../../../../presentation/protocols/validation';
+import { EmailValidator } from '../../../../presentation/protocols/email-validator';
+import { makeLoginValidation } from './login-validation-factory';
 
-jest.mock('../../../presentation/helpers/validators/validation-composite');
+jest.mock('../../../../presentation/helpers/validators/validation-composite');
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
@@ -21,16 +20,15 @@ const makeEmailValidator = (): EmailValidator => {
   return new EmailValidatorStub();
 };
 
-describe('SignUpValidation Factory', () => {
+describe('LoginValidation Factory', () => {
   it('should call ValidationComposite with all validations', () => {
-    makeSignUpValidation();
+    makeLoginValidation();
 
     const validations: Validation[] = [];
-    for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+    for (const field of ['email', 'password']) {
       validations.push(new RequiredFieldValidation(field));
     }
 
-    validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'));
     validations.push(new EmailValidation('email', makeEmailValidator()));
 
     expect(ValidationComposite).toHaveBeenCalledWith(validations);
---
+++ src/main/factories/decorators/log-controller-decorator-factory.ts
@@ -0,0 +1,9 @@
+import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository';
+import { Controller } from '../../../presentation/protocols';
+import { LogControllerDecorator } from '../../decorators';
+
+export const makeLogControllerDecorator = (controller: Controller): Controller => {
+  const logMongoRepository = new LogMongoRepository();
+
+  return new LogControllerDecorator(controller, logMongoRepository);
+};
