3c247ad7ece041484cf420e540e6f68cc0f574d0
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-07-25 13:14:41 -0300

    test: ensure load surveys route returns 200 if a valid token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -71,5 +71,25 @@ describe('Survey Routes', () => {
         .get('/api/surveys')
         .expect(403);
     });
+
+    it('should return 204 on load surveys with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Vinicius',
+        email: 'viniciussdsilva@gmail.com',
+        password: '123',
+      });
+      const id = res.ops[0]._id;
+      const accessToken = await jwt.sign({ id }, env.jwtSecret);
+      await accountCollection.updateOne({ _id: id }, {
+        $set: {
+          accessToken,
+        },
+      });
+
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .expect(204);
+    });
   });
 });
