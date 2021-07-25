716a1b1c4bcf4edafc10e35fdce50d1fd7e10a37
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-07-25 19:44:31 -0300

    feat: add LoadSurveyById usecase

--- src/domain/usecases/index.ts
+++ src/domain/usecases/index.ts
@@ -2,4 +2,5 @@ export * from './add-account';
 export * from './add-survey';
 export * from './authentication';
 export * from './load-account-by-token';
+export * from './load-survey-by-id';
 export * from './load-surveys';
