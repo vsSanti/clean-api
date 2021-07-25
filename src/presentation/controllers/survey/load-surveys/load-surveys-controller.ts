d65669cc182cab1c9b3a9fb3fbbd4bd7fad817dc
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-07-25 12:18:36 -0300

    feat: ensure LoadSurveysController returns 204 if LoadSurveysUseCase returns empty

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -4,7 +4,7 @@ import {
   HttpResponse,
   LoadSurveys,
 } from './load-surveys-controller-protocols';
-import { ok, serverError } from '../../../helpers/http/http-helper';
+import { noContent, ok, serverError } from '../../../helpers/http/http-helper';
 
 export class LoadSurveysController implements Controller {
   constructor (private readonly loadSurveys: LoadSurveys) { }
@@ -12,6 +12,8 @@ export class LoadSurveysController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const surveys = await this.loadSurveys.load();
+      if (!surveys.length) return noContent();
+
       return ok(surveys);
     } catch (error) {
       return serverError(error);
