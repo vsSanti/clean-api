db5aebf095a7bd8b65781301ea67f983a13397d0
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-07-25 23:21:08 -0300

    test: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct values

---
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -0,0 +1,60 @@
+import MockDate from 'mockdate';
+
+import { SurveyResultModel, SaveSurveyResultModel, SaveSurveyResultRepository } from './db-save-survey-result-protocols';
+import { DbSaveSurveyResult } from './db-save-survey-result';
+
+const makeFakeSurveyResultData = (): SaveSurveyResultModel => ({
+  accountId: 'any_account_id',
+  surveyId: 'any_survey_id',
+  answer: 'any_answer',
+  date: new Date(),
+});
+
+const makeFakeSurveyResult = (): SurveyResultModel => {
+  return Object.assign({}, makeFakeSurveyResultData(), { id: 'any_id' });
+};
+
+const makeSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
+  class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
+    async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+      return makeFakeSurveyResult();
+    }
+  }
+
+  return new SaveSurveyResultRepositoryStub();
+};
+
+type SutTypes = {
+  sut: DbSaveSurveyResult
+  saveSurveyResultRepositoryStub: SaveSurveyResultRepository
+}
+
+const makeSut = (): SutTypes => {
+  const saveSurveyResultRepositoryStub = makeSaveSurveyResultRepository();
+
+  const sut = new DbSaveSurveyResult(saveSurveyResultRepositoryStub);
+
+  return {
+    sut,
+    saveSurveyResultRepositoryStub,
+  };
+};
+
+describe('DbSaveSurveyResult Usecase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date());
+  });
+
+  afterAll(() => {
+    MockDate.reset();
+  });
+
+  it('should call SaveSurveyResultRepository with correct values', async () => {
+    const { sut, saveSurveyResultRepositoryStub } = makeSut();
+    const saveSpy = jest.spyOn(saveSurveyResultRepositoryStub, 'save');
+
+    await sut.save(makeFakeSurveyResultData());
+
+    expect(saveSpy).toHaveBeenCalledWith(makeFakeSurveyResultData());
+  });
+});
