25709c0f10ac1b46d10a18590cba1a5049def902
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-07-31 18:54:14 -0300

    feat: ensure DbLoadSurveyResult returns empty survey result if there is no result

--- src/main/factories/usecases/survey-result/save-survey-result/db-save-survey-result-factory.ts
+++ src/main/factories/usecases/survey-result/save-survey-result/db-save-survey-result-factory.ts
@@ -3,6 +3,6 @@ import { SaveSurveyResult } from '@/domain/usecases/survey-result';
 import { SurveyResultMongoRepository } from '@/infra/db/mongodb/survey-result/survey-result-mongo-repository';
 
 export const makeDbSaveSurveyResult = (): SaveSurveyResult => {
-  const surveyRepository = new SurveyResultMongoRepository();
-  return new DbSaveSurveyResult(surveyRepository);
+  const surveyResultMongoRepository = new SurveyResultMongoRepository();
+  return new DbSaveSurveyResult(surveyResultMongoRepository, surveyResultMongoRepository);
 };
